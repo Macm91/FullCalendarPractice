53a532ee5cd5bfcbe46df3eaa108ba372465103d
Macm91 <macm91.mc@gmail.com>
2022-01-11 11:00:59 -0500

    set up a timer.jsx and timerSlice.jsx file. Currently working on the reducers for the timer.

---
+++ src/features/Timer/timerSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    // days: 0,
+    // hours: 0,
+    // minutes: 0,
+    seconds: 60,
+};
+
+export const createSlice = createSlice({
+    name: "timer",
+    initialState,
+    reducers: {
+        decrement(state){
+            state.vallue--;
+        },
+        // addTime(state, action){
+            
+        // }
+    }
+})
