5c0654116af038a1495a0e97fb74e73e3b63cb44
Macm91 <macm91.mc@gmail.com>
2022-01-11 11:20:28 -0500

    created a switch router to accommodate incoming components in the app.jsx file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
-import { Switch, Route} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
+import { Routes ,Route, BrowserRouter } from 'react-router-dom';
+import Timer from "./features/Timer/Timer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-       <Switch>
-          <Route />
-       </Switch>
-      </header>
+    <div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World</h1>
+        </header>
+      </div>
+      <Routes>
+            <Route path="/timer" component= {Timer}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
